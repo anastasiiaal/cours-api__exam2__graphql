39c9260abe6315bca6ef9d676b7a2e3aca9d1f29
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-11-14 14:51:43 +0100

    add apollo package & create its instance

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css'
-import App from './App.jsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+  uri: 'http://localhost:8081/graphql',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
